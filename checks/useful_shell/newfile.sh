4b014285e2738248915a4dc5d04d5cdec844dc99
peetuskytta <77061872+peetuskytta@users.noreply.github.com>
2021-12-20 12:52:29 +0200

    Add files via upload

--- checks/useful_shell/newfile.sh
+++ checks/useful_shell/newfile.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-	if [ -e "input.keys" ]
-	then
-		rm input.keys
-	fi
+#########################################################################################
+#
+# This Shell script will create a [NAME].c based on your input. It will also 
+# add the 42 header and [#include "libft.h"].
+#
+# Let's assume you want to start coding the ft_memset.c funtion. Running this script
+# will ask you to name your function and after hitting enter the ft_memset.c file with
+# your 42 header is created and you are ready to edit your file.
+#
+# I recommend creating an alias to run this script, as it makes it so much faster.
+# 
+# Mine looks like this: [alias new='source ../checks/useful_shell/./newfile.sh']
+# Whenever I run the script it will create the file in the current directory.
+# 
+# Feel free to modify this file to your own needs. 
+# 
+#########################################################################################
 
-	touch input.keys
-	echo ":Stdheader" >> input.keys
-	echo "ddZZ" >> input.keys
 	echo "Name the function you want to create:"
 	read filename
-	vim -s input.keys $filename
 	echo "#include \"libft.h\"" >> $filename
-	echo >> $filename
-	vim $filename
-	rm input.keys
+	chmod 755 $filename
+	vim +:Stdheader $filename
+
+# This code below creates a copy to eval/ directory for later use in tests.	
+	cp -v $filename ./eval/test_$filename
+	vim +':%d | x' ./eval/test_$filename
--- checks/useful_shell/proto.sh
+++ checks/useful_shell/proto.sh
@@ -1,51 +1,67 @@
 #!/bin/bash
 
-###############################################################
+# This script will update your libft header file with the
+# correct prototypes. It will also update the source files in Makefile.
 #
-# This script will remove your previous libft header file!
-# Make sure you modify this file to fit your needs. For example
-#
-#
-# Make sure you check the awk search parameters to match your
-# needs.
-#
-###############################################################
+# Make sure you modify this file to fit your needs.
 
-
-	rm libft.h
 	echo "-------------------"
-	echo "Removing old libft.h file."
-
-# Here we add commands to input.keys file for the 42 header.
+#	Here we create the auxiliary files needed.
+	echo "Creating input.keys, func_p.txt and func.txt files "
 	touch input.keys
-	echo ":Stdheader" >> input.keys
-	echo "ddZZ" >> input.keys
+	touch input_l.keys
+	touch func_p.txt
+	touch func.txt 
 
-# Create new libft.h and add 42 header to it. Chmod might not be necessary.
-	echo "Creating new libft.h file."
-	echo "Adding the header."
-	vim -s input.keys libft.h
-	chmod 755 libft.h
-	rm input.keys
+# In the section below the script will find the prototypes, sort them based on 
+# the first column, save them to func_p.txt file where sort and formatting happens.
+# Also, any static function declarations are deleted.
+#
+# NOTE! Script will erase everything below line 18 in libft.h header file. So, if you
+# have anything else below the prototypes make sure to adjust this script.
+#
+# Another way would be using the vim command {:g/);/d} which deletes all lines containing
+# the /PATTERN/.
 
-# Including the stuff we need after the 42 header. Could use another method 
-# instead of echo? Add more includes and defines if you need.
-	echo "#ifndef LIBFT_H" >> libft.h
-	echo "# define LIBFT_H" >> libft.h
-	echo >> libft.h
-	echo "# include <string.h>" >> libft.h
-	echo "# include <unistd.h>" >> libft.h
-	echo "# include <stdlib.h>" >> libft.h
-	echo >> libft.h
-
-# This will search the prototypes, sort them based on the third column 
-# and print them to libft header file adding the semicolon in the end of each protoype.
-	echo "Adding the prototypes."
+	echo "Finding and formatting the prototypes."
 	awk '/void\011ft_/ || /int\011ft_/ || /char\011ft_/ || /size_t\011ft_/ \
-	|| /void\011\*ft_/ || /char\011\*ft_/ {print $0";"}' *ft_* | sort -k 3 >> libft.h
-
-# Adds whatever else you need after the prototypes.
-	echo >> libft.h
-	echo "#endif" >> libft.h
+	|| /void\011\*ft_/ || /char\011\*ft_/ || /int\011\*ft_/ || /char\011\**ft/ \
+	{print $0";"}' *ft_* > func_p.txt
+	vim +':sort | x' func_p.txt
+	vim +':%s/int\t/int\t\t/ | x' func_p.txt
+	echo "19gg" > input_l.keys
+	echo "dG" >> input_l.keys
+	echo ":r func_p.txt" >> input_l.keys
+	echo ":g/static/d" >> input_l.keys
+	echo "ZZ" >> input_l.keys
+	echo "Updating the header file."
+	vim -s input_l.keys libft.h
+	echo "\n#endif" >> libft.h
 	echo "Finished."
 	echo "-------------------"
+
+# In the section below we update the Makefile contents with the source .c files.
+# The script will erase the {SRC =} or whatever you have in the lines you specify
+# the source files.
+# Adjust the contents below to meet your needs.
+
+	echo "Finding and formatting the source files."
+	echo "SRC =" > func.txt
+	ls *ft_* | awk '/ft_/ {print $0" \\"}' >> func.txt
+	vim +':%s/ft_/\tft_/g | x' func.txt 
+	echo "ggJZZ" > input.keys
+	vim -s input.keys func.txt
+	echo ":g/ft_/d" > input.keys
+	echo "15gg" >> input.keys
+	echo ":r func.txt" >> input.keys
+	echo "ZZ" >> input.keys
+	echo "Updating the contents of Makefile"
+	vim -s input.keys Makefile
+	echo "All .c sources in Makefile updated and formatted."
+#	rm func.txt 
+#	rm func_p.txt
+	rm input.keys
+	rm input_l.keys
+	echo "Finished."
+	echo "-------------------"
+	
