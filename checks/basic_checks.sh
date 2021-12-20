2ef93274c4a2c86e85d1f75449d4d56f173a3d53
peetuskytta <77061872+peetuskytta@users.noreply.github.com>
2021-12-20 12:53:44 +0200

    Add files via upload

--- checks/basic_checks.sh
+++ checks/basic_checks.sh
@@ -6,26 +6,52 @@ file_ko="\033[31mKO\033[0m"
 file_found="\033[32mfound\033[0m"
 file_not_found="\033[31mnot found\033[0m"
 
-PATH_LIBFT=$"~/libft"
 COLOR_FAIL="\033[31m"
 COLOR_OK="\033[32m"
 COLOR_DEF="\033[0m"
+_dFILES="libft/*ft_*"
 
 printf "\n"
 printf "_______________________________________________"
+printf "\n"
+printf "\n"
+check_makefile
+printf "\n"
+printf "_______________________________________________"
 printf "\n\n"
+check_header
+check_author
+printf "\n"
+check_norm
+printf "\n"
+
+
 
 check_makefile()
 {
 	if [ -e "Makefile" ];
 	then
 		printf "Makefile"
-		printf "\033[15G-Wall"
-		printf "\033[25G-Wextra"
-		printf "\033[35G-Werror\n"
+		printf "\033[20G-Wall"
+		printf "\033[30G-Wextra"
+		printf "\033[40G-Werror\n"
 		printf "$file_found"
-		printf "\033[15G"
+		printf "\033[20G"
 		if [ -z "$(grep -w '\-Wall' Makefile)" ];
+		then
+			printf "${COLOR_FAIL}missing${COLOR_DEF}"
+		else
+			printf "${COLOR_OK}OK${COLOR_DEF}"
+		fi
+		printf "\033[30G"
+		if [ -z "$(grep -w '\-Wextra' Makefile)" ];
+		then
+			printf "${COLOR_FAIL}missing${COLOR_DEF}"
+		else
+			printf "${COLOR_OK}OK${COLOR_DEF}"
+		fi
+		printf "\033[40G"
+		if [ -z "$(grep -w '\-Werror' Makefile)" ];
 		then
 			printf "${COLOR_FAIL}missing${COLOR_DEF}"
 		else
@@ -50,17 +76,19 @@ check_author()
 {
 	if [ -f "author" ];
 	then
-    	printf "Author file\033[15G$file_ok\n"
+    	printf "Author file\033[15G$file_ok"
 	else
-	    printf "Author file\033[15G$file_ko\n"
-	fi
+	    printf "Author file\033[15G$file_ko"
+    fi
 }
 
-check_makefile
-printf "\n"
-printf "_______________________________________________"
-printf "\n\n"
-check_header
-printf "\n"
-check_author
-printf "\n"
+check_norm()
+{
+#	ls -l *ft_* | awk '/ft_/ {print $9}' > func.txt
+#	while IFS= read -r $_dfiles
+#	do
+#		[ -f "$_dfiles" ] && norminette -R CheckForbiddenFlags $d_files
+#
+#	done < "func.txt"
+#	norminette -R CheckForbiddenFlags *ft_* | awk '/Error/ {print}'
+}
